30b082f8186a143d7b9460963c8a8f6e99510b25
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-06 16:46:33 -0300

    test(backend): adiciona testes para a rota principal

--- backend/tests.py
+++ backend/tests.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+from app import app
 from weather_api import get_data_from_city, get_data_from_coordinates
 
 CIDADE_TESTE = "Belém"
@@ -15,6 +16,30 @@ class TestApiCall(unittest.TestCase):
     def test_success_on_get_data_from_coordinates(self):
         self.assertEqual(get_data_from_coordinates(
             LAT_TESTE, LON_TESTE)['cod'], 200)
+
+
+class TestHome(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_route(self):
+        response = self.app.get('/')
+        self.assertEqual(200, response.status_code)
+
+    def test_get_city(self):
+        response = self.app.get('/?cidade={}'.format(CIDADE_TESTE))
+        self.assertEqual(200, json.loads(response.data)['cod'])
+
+    def test_get_from_cache(self):
+        self.app.get('/?cidade={}'.format(CIDADE_TESTE))
+        response = self.app.get('/')
+        self.assertEqual(200, json.loads(response.data)['cod'])
+        self.assertEqual(CIDADE_TESTE, json.loads(response.data)['name'])
+
+    def test_content_type(self):
+        response = self.app.get('/')
+        self.assertIn('application/json', response.content_type)
 
 if __name__ == '__main__':
     unittest.main()
