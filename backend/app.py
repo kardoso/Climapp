7e16a896dd49f4d680d7ecc99948ed6c8872c3a0
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-06 15:48:59 -0300

    feat(backend): adiciona cache para salvar dados da última chamada da api

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request
+from flask_caching import Cache
+from unidecode import unidecode
 from weather_api import get_data_from_city, get_data_from_coordinates
 
 config = {
-    "DEBUG": True
+    "DEBUG": True,
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 15 * 60
 }
 
 app = Flask('Climapp')
 app.config.from_mapping(config)
+cache = Cache(app)
 
 
 @app.route('/')
@@ -16,16 +21,33 @@ def pagina_inicial():
     lon = request.args.get('lon', None)
 
     if cidade is not None:
-        data = get_data_from_city(cidade)
+        if (cache.get("climate_data") and
+            comparar_case_insensitive_unidecode(
+                cache.get("climate_data")["name"], cidade
+            )
+        ):
+            data = cache.get("climate_data")
+        else:
+            data = get_data_from_city(cidade)
+            cache.set("climate_data", data)
     else:
         if lat is not None and lon is not None:
-            data = get_data_from_coordinates(lat, lon)
+            if (lat == cache.get("climate_data")['coord']['lat'] and
+                lon == cache.get("climate_data")['coord']['lon']
+                ):
+                data = cache.get("climate_data")
+            else:
+                data = get_data_from_coordinates(lat, lon)
+                cache.set("climate_data", data)
         else:
-            data = {
+            data = cache.get("climate_data") or {
                 "warning": "No data was returned"
             }
 
     return data
+
+def comparar_case_insensitive_unidecode(str1, str2):
+    return unidecode(str1).lower() == unidecode(str2).lower()
 
 
 if __name__ == "__main__":
