76be79de967db6cd3f0de81793334be8c9733c7b
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-08 15:50:04 -0300

    feat(style/web): estiliza componente de busca

--- web/src/Components/InputLocal/index.js
+++ web/src/Components/InputLocal/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
