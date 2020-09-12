b1172d2dac6dd5189a8c90990f90251f4c538b7c
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-09-12 02:58:57 -0300

    feat(mobile): Adiciona imagem de fundo do app

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import { apiURI } from "./constants";
 import InfoClima from "./components/InfoClima";
 
@@ -19,6 +19,13 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Image
+        style={styles.backgroundImage}
+        source={{
+          uri:
+            "https://images.unsplash.com/photo-1583887547017-fbe85bd65ff2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=980&q=80",
+        }}
+      />
       <InfoClima data={estado} />
       <StatusBar style="auto" />
     </View>
@@ -28,8 +35,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  backgroundImage: {
+    resizeMode: "cover",
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    opacity: 0.6,
   },
 });
